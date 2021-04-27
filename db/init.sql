cc017315764b12c54738a014f93c3c3091c234fe
AlwaysEpicurus <me@davidpreciado.com>
2021-04-27 12:56:18 -0700

    Feature: Added Mileage & Price Column Data!

--- db/init.sql
+++ db/init.sql
@@ -4,8 +4,8 @@ use citiesData;
 CREATE TABLE IF NOT EXISTS tblCitiesImport (
     `id` int AUTO_INCREMENT,
     `year` INT,
-    `mileage` NUMERIC(6, 4),
-    `price` NUMERIC(7, 4),
+    `mileage` INT,
+    `price` INT,
     `fldCountry` VARCHAR(19) CHARACTER SET utf8,
     `fldAbbreviation` VARCHAR(3) CHARACTER SET utf8,
     `fldCapitalStatus` VARCHAR(7) CHARACTER SET utf8,
@@ -13,5 +13,5 @@ CREATE TABLE IF NOT EXISTS tblCitiesImport (
     PRIMARY KEY (`id`)
 );
 INSERT INTO tblCitiesImport (year,mileage,price,fldCountry,fldAbbreviation,fldCapitalStatus,fldPopulation) VALUES
-    (1998,35.685,139.7514,'Japan','JPN','primary',35676000),
-    (1997,39.9304,119.62,'China','CHN','NA',1003000);
+    (1998,27,9991,'Japan','JPN','primary',35676000),
+    (1997,17,9925,'China','CHN','NA',1003000);
